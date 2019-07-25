4c4f1dc94971c1331eb3786a0d5a66d758eda8c3
joeselD <vince.kristofer.canete@webee.com.mt>
2019-07-26 00:11:39 +0800

    Push Builder and Traits

---
+++ src/EncryptableAttribute.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * src/Traits/EncryptedAttribute.php.
+ *
+ * @author      JoeselDuazo <http://joeselduazo.com/>
+ * @version     v0.3.18
+ */
+
+namespace Magros\Encryptable;
+
+
+use Magros\Encryptable\Builders\EncryptionBuilder;
+use Illuminate\Support\Facades\Crypt;
+trait EncryptableAttribute {
+
+    public function getAttribute($key)
+    {
+      $value = parent::getAttribute($key);
+      if (in_array($key, $this->encryptable) && (!is_null($value) && $value != ''))
+      {
+        try {
+          $value = Crypt::decrypt($value);
+        } catch (\Exception $th) {}
+      }
+      return $value;
+    }
+
+    public function setAttribute($key, $value)
+    {
+      if (in_array($key, $this->encryptable))
+      {
+        try {
+          $value = Crypt::encrypt($value);
+        } catch (\Exception $th) {}
+      }
+      return parent::setAttribute($key, $value);
+    }
+
+    public function attributesToArray()
+    {
+        $attributes = parent::attributesToArray();
+        if ($attributes) {
+          foreach ($attributes as $key => $value)
+          {
+            if (in_array($key, $this->encryptable) && (!is_null($value)) && $value != '')
+            {
+              $attributes[$key] = $value;
+              try {
+                $attributes[$key] = Crypt::decrypt($value);
+              } catch (\Exception $th) {}
+            }
+          }
+        }
+        return $attributes;
+    }
+    
+    // Extend EncryptionBuilder
+    public function newEloquentBuilder($query)
+    {
+        return new EncryptionBuilder($query);
+    }
+}
